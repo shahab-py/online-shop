8969387d9a6777440b9dbedeaddd4a9af9db302c
shahab <ashahhab2727@gmail.com>
2023-02-01 14:50:27 +0330

    commiting online shop

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from .models import Product
+
+
+class HomeView(View):
+    def get(self,request):
+        products = Product.objects.filter(avalable = True)
+        return render(request, 'home/home.html', {'products':products})
+
+
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'home/detail.html', {'product':product})
