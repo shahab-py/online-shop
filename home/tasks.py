54f99233a5bf434baafa2ee9b6306973588276fa
shahab <ashahhab2727@gmail.com>
2023-02-25 21:42:08 +0330

    django storage & bucket

---
+++ home/tasks.py
@@ -0,0 +1,5 @@
+from bucket import bucket
+
+def all_bucket_objects_task():
+    result = bucket.get_objects()
+    return result
--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ app_name = 'home'
 
 urlpatterns = [
     path('',views.HomeView.as_view(), name = 'home'),
+    path('bucket/', views.BucketHome.as_view(), name = 'bucket'),
     path('<slug:slug>/', views.ProductDetailView.as_view(), name = 'product_detail'),
 ]
