1d3f8a95a5001e26d90272e2267c7b3b5ea297df
shahab <ashahhab2727@gmail.com>
2023-03-12 20:44:58 +0330

    nested category

--- home/views.py
+++ home/views.py
@@ -10,7 +10,7 @@ from utils import IsAdminUserMixin
 class HomeView(View):
     def get(self,request, category_slug=None):
         products = Product.objects.filter(avalable = True)
-        categories = Category.objects.all()
+        categories = Category.objects.filter(is_sub=False)
         if category_slug:
             category = Category.objects.get(slug=category_slug)
             products = products.filter(category=category)
