c7f1efff748ee41251b469f5df58ac75288f0f6d
shahab <ashahhab2727@gmail.com>
2023-03-19 23:22:54 +0330

    permissions

--- accounts/admin.py
+++ accounts/admin.py
@@ -20,15 +20,13 @@ class UserAdmin(BaseUserAdmin):
     list_filter = ('is_admin',)
     fieldsets = (
         ('Main', {'fields':('email', 'phone_number', 'full_name', 'password')}),
-        ('Permissions', {'fields':('is_active', 'is_admin', 'last_login')}),
+        ('Permissions', {'fields':('is_active', 'is_admin', 'is_superuser', 'last_login', 'groups', 'user_permissions')}),
     )
     add_fieldsets = (
         (None, {'fields':('email', 'phone_number', 'full_name', 'password1', 'password2')}),
     )
     search_field = ('email', 'full_name'),
     ordering = ('full_name'),
-    filter_horizontal = ()
+    filter_horizontal = ('groups', 'user_permissions')
 
-
-admin.site.unregister(Group)
 admin.site.register(User, UserAdmin)
